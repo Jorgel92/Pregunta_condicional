3d2280f9cb791a9fa9efc700d91c78f64fd3d9e6
Jorge Martinez <jorgel.mgtz92@gmail.com>
2024-10-03 01:14:23 -0600

    primer ejercicio de condicionales

---
+++ eres hermose.js
@@ -0,0 +1,8 @@
+var pregunta =prompt("¿Es usted hermose?")
+//var respuesta= true
+
+var resultado = "si" === pregunta //&& respuesta===true
+if ("si" === pregunta)
+    {console.log("Ciertamente") }
+
+else {console.log("No te creo")}
